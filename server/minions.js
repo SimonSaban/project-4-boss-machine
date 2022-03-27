cd3bbf4e7e4fa79104e7d6b3bb3fd7a44ce424a7
SimonS <simonsaban@yahoo.co.uk>
2022-03-27 17:22:42 +0100

    Complete bonus all tests passing

--- server/minions.js
+++ server/minions.js
@@ -42,10 +42,35 @@ minionsRouter.delete('/:minionId', (req, res, next) => {
         res.status(404);
     res.send();
 });
-/*
+ 
 minionsRouter.get('/:minionId/work', (req, res, next) => {
-    const minionWork = getAllFromDatabase('work').filter(id=> id===req.minion.id);
+    const minionWork = getAllFromDatabase('work').filter(task=> task.minionId === req.minion.id);
     res.send(minionWork);
-});*/
+});
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+   const newWork = addToDatabase('work', req.body)
+    res.status(201).send(newWork);
+});
+
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if(req.body.minionId === req.params.minionId) {
+    const updatedWork = updateInstanceInDatabase('work', req.body);
+    res.send(updatedWork);
+    } else {
+        res.status(400).send();
+    }
+});
+
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const workId = req.params.workId;
+    const deletedSuccessfully = deleteFromDatabasebyId('work', workId);
+    if(deletedSuccessfully) {
+        res.status(204); 
+    } else {
+        res.status(404);
+    }
+    res.send();
+});
 
 module.exports = minionsRouter;
